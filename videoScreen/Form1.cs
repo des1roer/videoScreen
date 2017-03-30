ef20f723683873229fc618e43bb532b75118c4a6
des1roer <des1roer@gmail.com>
2017-03-30 08:20:23 +0500

    2nd

--- videoScreen/Form1.cs
+++ videoScreen/Form1.cs
@@ -5,6 +5,9 @@ using System.Windows.Forms;
 using Microsoft.Expression.Encoder.ScreenCapture;
 using System.IO;
 
+using System.Threading.Tasks;
+using System.Timers;
+using System.Threading;
 
 namespace videoScreen
 {
@@ -12,6 +15,7 @@ namespace videoScreen
     {
         ScreenCaptureJob _screenCaptureJob;
         string file = Directory.GetCurrentDirectory() + "/test.wmv";
+        static System.Windows.Forms.Timer myTimer = new System.Windows.Forms.Timer();
         public Form1()
         {
             _screenCaptureJob = new ScreenCaptureJob();
@@ -36,6 +40,13 @@ namespace videoScreen
                 {
                     File.Delete(_screenCaptureJob.OutputScreenCaptureFileName);
                 }
+
+                myTimer.Tick += new EventHandler(timer1_Tick);
+
+                // Sets the timer interval to 5 seconds.
+                myTimer.Interval = 1000 * 30;
+                myTimer.Start();
+
                 _screenCaptureJob.Start();
             }
             catch (Exception error) {
@@ -43,8 +54,15 @@ namespace videoScreen
             }
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void timer1_Tick(object sender, System.EventArgs e)
         {
+
+            myTimer.Stop();
+
+            Console.WriteLine("Текущее время:  " +
+             DateTime.Now.ToLongTimeString());
+            _screenCaptureJob.Stop();
+
             _screenCaptureJob.Stop();
             var ffMpeg = new NReco.VideoConverter.FFMpegConverter();
 
@@ -54,12 +72,19 @@ namespace videoScreen
                 null, // autodetect by output file extension 
                 new NReco.VideoConverter.ConvertSettings()
                 {
-                    
+
                     //      CustomOutputArgs = " -filter_complex \"[0] yadif=0:-1:0,scale=iw*sar:ih,scale='if(gt(a,16/9),1280,-2)':'if(gt(a,16/9),-2,720)'[scaled];[scaled] pad=1280:720:(ow-iw)/2:(oh-ih)/2:black \" -c:v libx264 -c:a mp3 -ab 128k "
-                        CustomOutputArgs = " -vcodec msmpeg4v2  -c:v libx264 -crf 24 -r 20 -b:v 500k"
+                    CustomOutputArgs = " -vcodec msmpeg4v2  -c:v libx264 -crf 24 -r 20 -b:v 500k"
 
                 }
             );
+        }
+
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+       
             //       var ffMpeg = new NReco.VideoConverter.FFMpegConverter();
             //       ffMpeg.ConvertMedia(file, "output.mp4", Format.mp4);
 
